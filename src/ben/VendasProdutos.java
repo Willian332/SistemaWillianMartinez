f1d0e3fa01a7873b2009a83b385b5418c1c89eb6
Willian332 <willian.martinez@estudante.ifms.edu.br>
2025-09-10 20:34:45 -0300

    Coloquei os bens e os daos, primeiro commit

---
+++ src/ben/VendasProdutos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ben;
+
+/**
+ *
+ * @author user
+ */
+public class VendasProdutos {
+    
+}
